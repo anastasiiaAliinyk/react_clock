1e42781369b93e4b73ddc68f1f8464d48b7ecd3b
anastasiia <anastasiia.aliinyk@gmail.com>
2021-03-19 09:25:02 +0200

    Updates

---
+++ precache-manifest.9db7d9662c40ae626a1515e8ee46b65c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15f95a18102269c100f852ec82d7c966",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e806e85d57e8ad30c1ea",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e806e85d57e8ad30c1ea",
+    "url": "./static/js/main.b776a630.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
